cc59e167d0c4b0abe870f45fdacb47409e2c2812
Hassan-Walid <hassan.walid77@gmail.com>
2024-03-16 00:04:55 +0200

    second

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const socket = require("socket.io");
 const app = express();
@@ -7,8 +6,22 @@ const server = app.listen(PORT, () => {
   console.log("server is listening on http://localhost:" + PORT);
 });
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
+const path = require("path");
+
 const sio = socket(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.get("/style.css", (req, res) => {
+  res.sendFile(path.join(__dirname, "/style.css"));
+});
+app.get("/client.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client.js"));
+});
+
 sio.on("connection", (client) => {
   console.log("we have a new client as id ", client.id);
   client.on("message", function (data) {
